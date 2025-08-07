3ebd5582b5528a8ad72ea913ff4f03980e2d4ebc
YAO Kossia Bénédicte <k.yao1305@gmail.com>
2025-08-07 16:05:34 +0000

    resolve review

--- ada-fe/tailwind.config.js
+++ ada-fe/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
           '0%': { opacity: '0', transform: 'scale(0.95)' },
           '100%': { opacity: '1', transform: 'scale(1)' },
         },
-      },},
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
